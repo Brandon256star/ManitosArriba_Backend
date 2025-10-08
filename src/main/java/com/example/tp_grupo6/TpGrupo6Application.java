aad36499342ff864f3ed3a51d77dd3f4f3fd4950
Batsi97 <proanosebastian6@gmail.com>
2025-10-08 01:54:44 -0500

    Primer commit limpio

---
+++ src/main/java/com/example/tp_grupo6/TpGrupo6Application.java
@@ -0,0 +1,13 @@
+package com.example.tp_grupo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGrupo6Application.class, args);
+    }
+
+}
